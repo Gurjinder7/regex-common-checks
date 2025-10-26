6ab85a0e0df4d6c82316269b8f942ed067a3eac4
Gurjinder <dev.gurjinder@gmail.com>
2025-10-26 15:38:47 +0000

    doc updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@
  * @example - containAnyDigits("!@#asdasd")
  * @description - Checks if the pattern contains any digit
  */
-
 export const containAnyDigits = (pattern) => {
     const checker = /[\d]/g
     return checker.test(pattern)
@@ -17,7 +16,6 @@ export const containAnyDigits = (pattern) => {
  * @example - firstDigitMatchIndex("asd4asd")
  * @description - Returns the first index of the digit in the pattern OR -1 if no digit is found
  */
-
 export const firstDigitMatchIndex = (pattern) => {
     const checker = /[\d]/g
     const checkArray = checker.exec(pattern)
@@ -33,7 +31,6 @@ export const firstDigitMatchIndex = (pattern) => {
  * @example - containsAnyNonWordCharacter("123as3 asd")
  * @description - checks if pattern contains any symbols (includes white space)
  */
-
 export const containsAnyNonWordCharacter = (pattern) => {
     const checker = /\W/g
     return checker.test(pattern)
@@ -45,7 +42,6 @@ export const containsAnyNonWordCharacter = (pattern) => {
  * @example - firstNonWordMatchIndex("asdasd dsa")
  * @description - Returns the first index of the non word symbol in the pattern OR -1 if none is found
  */
-
 export const firstNonWordMatchIndex = (pattern) => {
     const checker = /\W/g
     const checkArray = checker.exec(pattern)
@@ -61,7 +57,6 @@ export const firstNonWordMatchIndex = (pattern) => {
  * @example - containOnlyLetters("as$dasd")
  * @description - checks if pattern contains only letters and No digit or a special character
  */
-
 export const containOnlyLetters = (pattern) => {
     const checker = /\b[a-zA-Z]{1,}\b/g
     return checker.test(pattern) && !containsAnySymbol(pattern)
@@ -74,7 +69,6 @@ export const containOnlyLetters = (pattern) => {
  * @example - containsAnyWhiteSpace("sdas sd")
  * @description - checks if pattern contains any white space
  */
-
 export const containsAnyWhiteSpace = (pattern) => {
     const checker = /\s/g
     return checker.test(pattern)
@@ -87,7 +81,6 @@ export const containsAnyWhiteSpace = (pattern) => {
  * @example - firstWhiteSpaceMatchIndex("asd asd")
  * @description - Returns first matched index of a white space in the pattern or else -1
  */
-
 export const firstWhiteSpaceMatchIndex = (pattern) => {
     const checker = /\s/g
     const checkArray = checker.exec(pattern)
@@ -103,7 +96,6 @@ export const firstWhiteSpaceMatchIndex = (pattern) => {
  * @example - containsNoSymbol("asd#asd")
  * @description - Checks if the pattern is contains digits or letters or combinations only
  */
-
 export const containsNoSymbol = (pattern) => {
     const checker = /^[0-9a-zA-Z]+$/g
     return checker.test(pattern) 
@@ -115,7 +107,6 @@ export const containsNoSymbol = (pattern) => {
  * @example - containAnySymbol("asdasd!")
  * @description - Checks if the pattern contains any symbol
  */
-
 export const containsAnySymbol = (pattern) => {
     const checker = /[~`!@#$%^&*()_\-=+{[\]}\|\:;'"\?\/>\.<,]/g
     return checker.test(pattern)
@@ -128,7 +119,6 @@ export const containsAnySymbol = (pattern) => {
  * @example - containsThisSymbol("jkhgjhg#kjkjaasd",'#')
  * @description - Checks if the pattern contains the symbol mentioned
  */
-
 export const containsThisSymbol = (searchString, pattern) => {
     const checker = new RegExp(`[\\${pattern}]`);
 
@@ -142,7 +132,6 @@ export const containsThisSymbol = (searchString, pattern) => {
  * @example - firstSymbolMatchIndex("#","asdasd#")
  * @description - Checks if the pattern contains the symbol and returns its first matched index or else -1
  */
-
 export const firstSymbolMatchIndex = (searchString, pattern) => {
     const checker = new RegExp(`[\\${pattern}]`);
     const checkArray = checker.exec(searchString)
@@ -160,7 +149,6 @@ export const firstSymbolMatchIndex = (searchString, pattern) => {
  * @example - containsThisOrThatSymbol("123asd#sd123",'#','$')
  * @description - checks if pattern contains one of the symbol 
  */
-
 export const containsThisOrThatSymbol = (searchString, symbol1, symbol2) => {
     const checker = new RegExp(`[\\${symbol1}]|[\\${symbol2}]`);
     return checker.test(searchString)
@@ -172,7 +160,6 @@ export const containsThisOrThatSymbol = (searchString, symbol1, symbol2) => {
  * @example - startsWithADigitAndEndsWithALetter("123asdasdasd")
  * @description - Checks if a pattern is strictly alphanumeric (no symbols) starting with a digit and ends with a letter
  */
-
 export const startsWithADigitAndEndsWithALetter = (pattern) => {
     const checker = /\b[0-9]{1,}[a-zA-Z]{1,}\b/g
     return checker.test(pattern) && containsNoSymbol(pattern)
@@ -184,7 +171,6 @@ export const startsWithADigitAndEndsWithALetter = (pattern) => {
  * @example - startsWithALetterAndEndsWithADigit("asdasd123")
  * @description - Checks if a pattern is strictly alphanumeric (no symbols) starting with a letter and ends with a digit
  */
-
 export const startsWithALetterAndEndsWithADigit = (pattern) => {
     const checker = /\b[a-zA-Z]{1,}[0-9]{1,}\b/g
     return checker.test(pattern) && containsNoSymbol(pattern)
@@ -196,7 +182,6 @@ export const startsWithALetterAndEndsWithADigit = (pattern) => {
  * @example - startsWithADigitAndEndsWithADigit("123asdasd123")
  * @description - Checks if a pattern is strictly alphanumeric (no symbols) starting with a digit and ends with a digit
  */
-
 export const startsWithADigitAndEndsWithADigit = (pattern) => {
     const checker = /\b[0-9]{1,}[a-zA-Z]{1,}[0-9]{1,}\b/g
     return checker.test(pattern) && containsNoSymbol(pattern)
@@ -207,9 +192,7 @@ export const startsWithADigitAndEndsWithADigit = (pattern) => {
  * @returns boolean (true/false)
  * @example - startsWithALetterAndEndsWithALetter("sdf223asdfasd")
  * @description - Checks if a pattern is strictly alphanumeric (no symbols) starting with a letter and ends with a letter
- 
  */
-
 export const startsWithALetterAndEndsWithALetter = (pattern) => {
     const checker = /\b[a-zA-Z]{1,}[0-9]{1,}[a-zA-Z]{1,}\b/g
     return checker.test(pattern) && containsNoSymbol(pattern)
